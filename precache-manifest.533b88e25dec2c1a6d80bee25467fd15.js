c0bb6290d1937da898b8880ccc5dffa28abb6486
Ganeshrockz <ganeshrockz@github.com>
2022-10-24 14:12:20 +0530

    Updates

---
+++ precache-manifest.533b88e25dec2c1a6d80bee25467fd15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3aafe1bc6d01cf6bafa",
+    "url": "/WT20-2022/static/css/main.31e8e36b.chunk.css"
+  },
+  {
+    "revision": "d3aafe1bc6d01cf6bafa",
+    "url": "/WT20-2022/static/js/main.d3aafe1b.chunk.js"
+  },
+  {
+    "revision": "8391c5f3846102535d31",
+    "url": "/WT20-2022/static/js/runtime~main.8391c5f3.js"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/WT20-2022/static/css/2.801b82c9.chunk.css"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/WT20-2022/static/js/2.0be92d95.chunk.js"
+  },
+  {
+    "revision": "b204baae1186ea9b8435436a776d8bda",
+    "url": "/WT20-2022/index.html"
+  }
+];
